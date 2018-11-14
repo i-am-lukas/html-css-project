57af917c9afa84eb7dd5716e29975779d595f898
nanoDW <s.fluder@interia.eu>
2018-11-14 21:00:43 +0100

    transition property is not working

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
-function show() {
+function displayNavBar() {
     var actual = document.getElementById("navItems");
 
     if (actual.style.display == "none") {
         actual.style.display = "block";
-        actual.style.transition = "all 0.5s"; // not working
+        // actual.style.transitionProperty = "display";             transition property is not working
+        // actual.style.transitionDuration = "0.5s";
+        // actual.style.transitionTimingFunction = "ease-in-out";
+        window.addEventListener("resize", function () {
+            if (window.matchMedia("(max-width: 767px)").matches) {
+                actual.style.display = "block";
+
+            } else {
+                actual.style.display = "flex";
+            }
+        });
     } else {
         actual.style.display = "none";
-        actual.style.transition = "all 0.5s"; // not working, why?
+        window.addEventListener("resize", function () {
+
+            if (window.matchMedia("(max-width: 767px)").matches) {
+                actual.style.display = "none";
+            } else {
+                actual.style.display = "flex";
+            }
+        });
     }
 }
