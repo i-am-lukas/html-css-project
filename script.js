7039209020e1f469e1c9ffcf2ab1c87a519105f8
nanoDW <s.fluder@interia.eu>
2018-11-21 16:07:17 +0100

    fixed width

--- script.js
+++ script.js
@@ -3,13 +3,9 @@ function displayNavBar() {
 
     if (actual.style.display == "none") {
         actual.style.display = "block";
-        // actual.style.transitionProperty = "display";             transition property is not working
-        // actual.style.transitionDuration = "0.5s";
-        // actual.style.transitionTimingFunction = "ease-in-out";
         window.addEventListener("resize", function () {
             if (window.matchMedia("(max-width: 767px)").matches) {
                 actual.style.display = "block";
-
             } else {
                 actual.style.display = "flex";
             }
@@ -17,7 +13,6 @@ function displayNavBar() {
     } else {
         actual.style.display = "none";
         window.addEventListener("resize", function () {
-
             if (window.matchMedia("(max-width: 767px)").matches) {
                 actual.style.display = "none";
             } else {
